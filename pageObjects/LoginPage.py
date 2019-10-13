a81bfa68452dcd4015d0a1e53170b1ed1e7071c4
Eugen <anghel.bogdan.eugen@gmail.com>
2019-10-13 23:17:28 +0300

    update

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -19,3 +19,10 @@ class LoginPage():
         self.driver.find_element_by_xpath("//a[@href='https://www.pcgarage.ro/memorii/gskill/trident-z-rgb-64gb-ddr4-3000mhz-cl14-135v-dual-channel-kit/']").click()
     def memCosClick(self):
         self.driver.find_element_by_xpath("//div[@class='ps-addtocart']//button[@type='submit']").click()
+    def vcClick(self):
+        self.driver.find_element_by_xpath("//a[@href='https://www.pcgarage.ro/placi-video/'][text()='Placi video']").click()
+    def vcSelClick(self):
+        self.driver.find_element_by_xpath("//a[@href='https://www.pcgarage.ro/placi-video/inno3d/geforce-rtx-2080-super-twin-x2-oc-8gb-gddr6-256-bit/']").click()
+    def vcCosClick(self):
+        self.driver.find_element_by_xpath("//button[@aria-label='Adauga Inno3D GeForce RTX 2080 SUPER Twin X2 OC 8GB GDDR6 256-bit in cos']").click()
+        
--- testCases/buildNewPc.py
+++ testCases/buildNewPc.py
@@ -12,11 +12,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from pageObjects.LoginPage import LoginPage
 
+
+# This test make you(buy) the best computer, prepare your wallet!
 class LoginTest(unittest.TestCase):
     baseURL = "https://www.pcgarage.ro/"
     driver = webdriver.Chrome(executable_path = "F:/Python/Selenium-Python/drivers/chromedriver.exe")
-
-    
 
     @classmethod
     def setUpClass(cls):
@@ -37,19 +37,15 @@ class LoginTest(unittest.TestCase):
         self.assertTrue(procHeader == "Procesoare")        
         procSel = self.driver.find_element_by_xpath("//p[text()='Producator']//following-sibling::p[@class='lc-filter-char']//span[@class='visuallyhidden']").click()
         intelProc = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='https://www.pcgarage.ro/procesoare/intel/']"))).click()
-
         drpElement=self.driver.find_element_by_id("sortsel")
         drp=Select(drpElement)
         drp.select_by_visible_text("Pret descrescator")
         time.sleep(1)
         intelProcHeader = wait.until(EC.presence_of_element_located((By.XPATH, "//b[text()='Procesoare Intel']"))).text
         self.assertTrue(intelProcHeader == "Procesoare Intel") 
-
         intelProcSel = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='https://www.pcgarage.ro/procesoare/intel/coffee-lake-core-i9-9900k-360ghz-box/']"))).click()
-
         intelProcSelHeader = wait.until(EC.visibility_of_element_located((By.XPATH, "//h1[text()='Procesor Intel Coffee Lake, Core i9 9900K 3.6GHz box']"))).text
         self.assertTrue(intelProcSelHeader == "Procesor Intel Coffee Lake, Core i9 9900K 3.6GHz box")
-
         cosProcIntel = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit'][@aria-label='Adauga Intel Coffee Lake, Core i9 9900K 3.6GHz box in cos']"))).click()
         cosMeuDeCump = wait.until(EC.visibility_of_element_located((By.XPATH, "//h1[text()='Cosul tau de cumparaturi']"))).text
         self.assertTrue(cosMeuDeCump == "Cosul tau de cumparaturi")
@@ -75,9 +71,25 @@ class LoginTest(unittest.TestCase):
         memBuyHeader = wait.until(EC.presence_of_element_located((By.XPATH, "//h1[text()='Memorie G.Skill Trident Z RGB 64GB DDR4 3000MHz CL14 1.35v Dual Channel Kit']"))).text
         self.assertTrue(memBuyHeader == "Memorie G.Skill Trident Z RGB 64GB DDR4 3000MHz CL14 1.35v Dual Channel Kit")
         lp.memCosClick() 
-        
+        memCosDeCump = self.driver.find_element_by_xpath("//a[@href='https://www.pcgarage.ro/memorii/gskill/trident-z-rgb-64gb-ddr4-3000mhz-cl14-135v-dual-channel-kit/']")
+        self.assertTrue(cosMeuDeCump == "Cosul tau de cumparaturi")
+        self.assertIsNotNone(procCosDeCump)
+        self.assertIsNotNone(pbCosDeCump)
+        self.assertIsNotNone(memCosDeCump) 
 
-        
+        # Buy Video Card
+        lp.clickMainBth()
+        lp.vcClick()
+        cvHeader = wait.until(EC.presence_of_element_located((By.XPATH, "//b[text()='Placi video']"))).text
+        self.assertTrue(cvHeader == "Placi video")
+        lp.vcSelClick()
+        lp.vcCosClick()
+        vcCosDeCump = wait.until(EC.presence_of_element_located((By.XPATH, "//a[@href='https://www.pcgarage.ro/placi-video/inno3d/geforce-rtx-2080-super-twin-x2-oc-8gb-gddr6-256-bit/']"))).text
+        self.assertTrue(cosMeuDeCump == "Cosul tau de cumparaturi")
+        self.assertIsNotNone(procCosDeCump)
+        self.assertIsNotNone(pbCosDeCump)
+        self.assertIsNotNone(memCosDeCump)
+        self.assertIsNotNone(vcCosDeCump)
       
 
         time.sleep(5)
